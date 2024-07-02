85da98341252c9f060aa2662a52d0efcbece50cd
mazharul007 <mohammad.mazharul.hoque.007@gmail.com>
2024-07-03 01:31:04 +0600

    covering python fundamentals

---
+++ hello.py
@@ -0,0 +1,17 @@
+# Fundamentals 
+
+name = "Mohammad Mazharul Hoque"
+age = 30
+
+print("Hello my name is", name , "and my age is",  age)
+print(type(name))
+print(type(age))
+
+
+# print sum
+
+numOne = 50
+numTwo = 50
+
+sum = numOne + numTwo;
+print(sum);
